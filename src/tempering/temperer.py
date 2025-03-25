427431c12382aaf159e5a98863f93be56972c60e
Kirill Khoruzhii <khoruzhii.ka@gmail.com>
2025-03-25 03:48:41 +0100

    feat: add greedy graph coloring, add color-based metropolis parallel step, add examples of colored upd usage

--- src/tempering/temperer.py
+++ src/tempering/temperer.py
@@ -2,10 +2,10 @@ import torch
 from .metropolis import metropolis_step
 
 
-class Temperer(torch.nn.Module):
+class Temperer():
     """Class to perform parallel tempering optimization using Metropolis steps."""
 
-    def __init__(self, N, K, Q, beta, dtype=torch.float32, device=torch.device("cpu")):
+    def __init__(self, N, K, Q, beta, dtype=torch.float32, device=torch.device("cpu"), CIDX=None):
         """Initialize Temperer class.
 
         Args:
@@ -16,7 +16,6 @@ class Temperer(torch.nn.Module):
             dtype (torch.dtype, optional): Data type for tensors. Defaults to torch.float16.
             device (torch.device, optional): Device for computations. Defaults to CPU.
         """
-        super(Temperer, self).__init__()
 
         self.N = N
         self.K = K
@@ -28,16 +27,34 @@ class Temperer(torch.nn.Module):
 
         self.Q = Q.to(device, dtype=dtype).contiguous()  # Interaction matrices
         self.W = self.Q.size(0)
+        
+        self.CIDX = CIDX
+        self.ncolors = max(CIDX.keys()) + 1 if CIDX is not None else None
 
         # Initialize random binary states: shape (W, L, K, N)
         self.states = torch.randint(0, 2, (self.W, self.L, self.K, self.N),
                                     device=self.device, dtype=self.dtype)
+        
+        self.log = {}
 
     def step(self):
         """Perform one Metropolis step for state updates."""
         metropolis_step(self.states.contiguous(),
-                        self.Q, self.beta, self.W, self.L, self.K, self.N,
-                        torch.seed())
+                            self.Q, self.beta, self.W, self.L, self.K, self.N,
+                            torch.seed())
+            
+    def step_colored(self):
+        """Perform one parallel metropolis step for state updates according to coloring"""
+        for j in torch.randperm(self.ncolors).tolist():
+            h = torch.einsum("wij, wlkj -> wlki", self.Q, self.states)
+            h = h[self.CIDX[j]['w'], :, :, self.CIDX[j]['n']]
+            x = 1 - 2 * self.states[self.CIDX[j]['w'], :, :, self.CIDX[j]['n']]
+            dE = 2 * h * x + self.Q[self.CIDX[j]['w'], self.CIDX[j]['n'], self.CIDX[j]['n'], None, None]
+            accept = torch.less(
+                torch.rand_like(dE),
+                torch.exp(-self.beta.view(1, self.L, 1) * dE.clip(0))
+            ) * x
+            self.states[self.CIDX[j]['w'], :, :, self.CIDX[j]['n']] += accept
 
     def energy(self):
         """Compute energies of the current states.
